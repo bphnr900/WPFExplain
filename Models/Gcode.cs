73b3c48a1bd0f7336b98cb484e118f5f66836887
bphnr900 <40142616+bphnr900@users.noreply.github.com>
2020-07-08 00:42:55 +0900

    Add files via upload

---
+++ Models/Gcode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpfSample04.Models
+{
+    class Gcode
+    {
+        public int ID { get; set; }
+        public string Code { get; set; }
+        public string Model { get; set; }
+    }
+}
---
+++ Models/GcodeStore.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace wpfSample04.Models
+{
+    class GcodeStore
+    {
+        public ObservableCollection<Gcode> Gcodes { get; set; }
+
+        public GcodeStore()
+        {
+            this.Gcodes = new ObservableCollection<Gcode>();
+            this.Gcodes = new ObservableCollection<Gcode>(Enumerable.Range(0, 99).Select(i => new Gcode
+            {
+                ID = i,
+                Model = i % 2 == 0 ? "501F" : "701F",
+                Code = i % 2 == 0 ? "G5FQTRA" + i.ToString() : "G7FQTRA" + i.ToString(),
+            }).ToList());
+        }
+    }
+}
